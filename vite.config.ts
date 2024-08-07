3ee4d27f29b5c800d88446c6dcb6a2d18a37e980
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-08-07 15:16:44 +0200

    install dependencies, alias config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@data": path.resolve(__dirname, "src/data"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@redux": path.resolve(__dirname, "src/redux"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@typings": path.resolve(__dirname, "src/typings"),
+    },
+  },
   plugins: [react()],
-})
+});
