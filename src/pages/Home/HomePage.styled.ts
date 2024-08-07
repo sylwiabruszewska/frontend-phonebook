01d643d590cf80f98a3161ecd870957b51fece27
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-08-07 18:53:06 +0200

    refactor HomePage

---
+++ src/pages/Home/HomePage.styled.ts
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+
+export const StyledHome = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 10px;
+  width: 100%;
+
+  @media (min-width: 1200px) {
+    flex-direction: row-reverse;
+    justify-content: space-between;
+    gap: 1rem;
+    width: 80%;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    @media (min-width: 1200px) {
+      padding: 0px 20px;
+      width: 60%;
+    }
+  }
+
+  h1 {
+    font-family: "Galada", sans-serif;
+    color: #fb8e5d;
+    margin: 2rem 0;
+    font-size: 3rem;
+    text-align: center;
+  }
+`;
+
+export const StyledImg = styled.img`
+  width: 80%;
+  height: auto;
+  object-fit: contain;
+
+  @media (min-width: 768px) {
+    width: 400px;
+  }
+
+  @media (min-width: 1200px) {
+    width: 500px;
+  }
+`;
+
+export const StyledList = styled.ul`
+  margin-bottom: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  @media (min-width: 1200px) {
+    flex-direction: row;
+    gap: 20px;
+    margin-bottom: 4rem;
+  }
+`;
+
+export const StyledListItem = styled.li`
+  flex: 1 1 0;
+  margin-bottom: 1.2rem;
+  text-align: center;
+
+  span {
+    font-size: 2em;
+    text-align: center;
+    color: #a06cd5;
+    margin-bottom: 0.6rem;
+  }
+
+  h3 {
+    margin-bottom: 1rem;
+  }
+`;
+
+export const StyledParagraph = styled.p`
+  margin: 1.2rem 0;
+
+  a {
+    font-weight: 500;
+    color: #513072;
+
+    &:hover {
+      color: #a06cd5;
+    }
+  }
+`;
---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,85 @@
+import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
+import { BsStars } from "react-icons/bs";
+import { FaCloud, FaBook } from "react-icons/fa";
+
+import {
+  StyledHome,
+  StyledList,
+  StyledListItem,
+  StyledParagraph,
+  StyledImg,
+} from "./HomePage.styled";
+import { Button } from "@components/Button/Button";
+import photo from "@assets/homepage.webp";
+
+const Home = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Phonebook App</title>
+      </Helmet>
+      <StyledHome>
+        <StyledImg
+          src={photo}
+          alt="A woman and a man standing and talking with a telephone book in the background."
+          role="presentation"
+          aria-hidden="true"
+        />
+        <div>
+          <h1>Phonebook App</h1>
+
+          <StyledList>
+            <StyledListItem>
+              <span>
+                <FaBook />
+              </span>
+              <h3>Keep your contacts close</h3>
+              <p>
+                No more worries about losing important phone numbers. Stay
+                connected with ease.
+              </p>
+            </StyledListItem>
+
+            <StyledListItem>
+              <span>
+                <BsStars />
+              </span>
+              <h3>Simplified Contact Management</h3>
+              <p>
+                Our user-friendly interface makes it simple to add and manage
+                your contacts with just a few clicks.
+              </p>
+            </StyledListItem>
+
+            <StyledListItem>
+              <span>
+                <FaCloud />
+              </span>
+              <h3>Access Your Contacts Anywhere</h3>
+              <p>
+                Create your account and securely store all your contacts in one
+                place.
+              </p>
+            </StyledListItem>
+          </StyledList>
+
+          <Link to="/register">
+            <Button type="button" aria-label="register">
+              Get Started Now
+            </Button>
+          </Link>
+
+          <StyledParagraph>
+            <span>Already a member? </span>
+            <Link to="/login" aria-label="login">
+              Sign in
+            </Link>
+          </StyledParagraph>
+        </div>
+      </StyledHome>
+    </>
+  );
+};
+
+export default Home;
