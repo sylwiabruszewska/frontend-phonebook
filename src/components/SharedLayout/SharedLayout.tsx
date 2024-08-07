ae0464ea6c831776fc6980b089f5a930ff5eb48a
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-08-07 18:41:05 +0200

    refactor shared layout component

---
+++ src/components/SharedLayout/SharedLayout.tsx
@@ -0,0 +1,31 @@
+import { Outlet } from "react-router-dom";
+import { Suspense } from "react";
+import { useSelector } from "react-redux";
+
+import { Loader } from "@components/Loader/Loader";
+import { Navigation } from "@components/Navigation/Navigation";
+import { Content } from "@components/Content/Content";
+import { Modal } from "@components/Modal/Modal";
+
+import { StyledHeader } from "./SharedLayout.styled";
+import { selectModalOpen } from "@redux/modal/selectors";
+
+export const SharedLayout = () => {
+  const modalOpen = useSelector(selectModalOpen);
+
+  return (
+    <>
+      <StyledHeader>
+        <Navigation />
+      </StyledHeader>
+
+      <Content>
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </Content>
+
+      {modalOpen && <Modal />}
+    </>
+  );
+};
