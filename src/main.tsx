fec040a9be40cfdb6c1b57c18a410e6c43e6072a
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2024-08-08 19:22:58 +0200

    add notiflix config

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "normalize.css";
 import "./index.css";
+import "@utils/notiflixConfig";
 import { Provider } from "react-redux";
 import { store, persistor } from "@redux/store";
 import { BrowserRouter } from "react-router-dom";
@@ -10,13 +11,13 @@ import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
+  // </React.StrictMode>
 );
---
+++ src/utils/notiflixConfig.ts
@@ -0,0 +1,13 @@
+import Notiflix from "notiflix";
+
+Notiflix.Notify.init({
+  width: "300px",
+  position: "center-top",
+  timeout: 5000,
+  success: {
+    background: "#a06cd5",
+  },
+  warning: {
+    background: "#fb8e5d",
+  },
+});
